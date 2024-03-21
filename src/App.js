f51af0346b7590405193b50b4c3071f11848abb2
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-22 02:21:59 +0900

    [feat] App 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-function App() {
+import styled from "styled-components";
+import Edit from "./components/Edit";
+import Current from "./components/Current";
+import EditModal from "./components/EditModal";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const initiate = JSON.parse(localStorage.getItem("testify")) || [];
+    setList(initiate);
+  }, []);
+
+  function handleOpenModal() {
+    setOpen((open) => !open);
+  }
+
   return (
-    <div className="App">
-      <h1>CEOS 19기 프론트엔드 파이팅!( ¨̮ )و✧🔥</h1>
-    </div>
+    <>
+      <Header>TODO LIST</Header>
+      <Current list={list} />
+      <hr />
+      <Edit list={list} setList={setList} />
+      <Footer>
+        <FooterButton onClick={handleOpenModal}>ADD</FooterButton>
+      </Footer>
+      {open && (
+        <EditModal
+          handleOpenModal={handleOpenModal}
+          list={list}
+          setList={setList}
+        />
+      )}
+    </>
   );
 }
 
-export default App;
+const Header = styled.h1`
+  padding: 1.5rem;
+  border-bottom: 1px solid black;
+  background-color: ${({ theme }) => theme.colors.mainbg};
+  text-align: center;
+`;
+
+const Footer = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 0.5rem 0;
+  background-color: ${({ theme }) => theme.colors.mainbg};
+  text-align: center;
+`;
+
+const FooterButton = styled.button`
+  width: 4rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  background-color: ${({ theme }) => theme.colors.commonbg};
+  color: white;
+  ${({ theme }) => theme.fonts.eng};
+`;
