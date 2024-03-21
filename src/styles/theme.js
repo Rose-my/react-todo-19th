272d321b9cc9d7d06ae999c1dc5bc1d7f2aecd71
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-22 02:18:47 +0900

    [design] theme 설정

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const colors = {
+  white: "#FFFFFF",
+  gray: "#555555",
+  black: "#000000",
+  point: "#DFE4FF",
+  schedule: "#ff0000",
+  done: "#0000ff",
+  commonbg: "rgb(33 109 176)",
+  mainbg: "rgb(134 196 173)",
+  modalbg: "rgb(0 0 0 / 70%)",
+};
+
+const fonts = {
+  eng: "font-family: 'Gaegu', sans-serif;",
+};
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
