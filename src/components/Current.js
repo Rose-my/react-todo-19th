b41caffc97cd7a1f894fb10944f4f4083a1e9a80
Rose <120544840+Rose-my@users.noreply.github.com>
2024-03-22 02:21:44 +0900

    [feat] Current 구현

---
+++ src/components/Current.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+
+export default function Current(props) {
+  const { list } = props;
+  const [countList, setCountList] = useState({ schedule: 0, done: 0 });
+
+  useEffect(() => {
+    function handleCount() {
+      let scheduleCount = 0;
+      let doneCount = 0;
+
+      list.forEach((data) => {
+        if (data.schedule) {
+          scheduleCount++;
+        } else {
+          doneCount++;
+        }
+      });
+      setCountList({ schedule: scheduleCount, done: doneCount });
+    }
+    handleCount();
+  }, [list]);
+
+  return (
+    <Section>
+      <Header>Current</Header>
+      <DetailBox>
+        <Text $schedule={true}>SCHEDULE</Text>
+        <Count $schedule={true}>{countList.schedule}</Count>
+        <Text $schedule={false}>DONE</Text>
+        <Count $schedule={false}>{countList.done}</Count>
+      </DetailBox>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin: 1rem 3rem;
+  padding: 1rem;
+  border: 3px solid black;
+  border-radius: 0.7rem;
+  background-color: ${({ theme }) => theme.colors.mainbg};
+`;
+
+const Header = styled.h2`
+  font-size: 2rem;
+  font-weight: 600;
+`;
+
+const DetailBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
+  margin: 0.7rem;
+  font-size: 1.4rem;
+`;
+
+const Text = styled.p`
+  display: inline-block;
+  border-radius: 2rem;
+  font-weight: 600;
+  text-align: center;
+  color: ${({ theme, $schedule }) =>
+    $schedule ? theme.colors.schedule : theme.colors.done};
+`;
+
+const Count = styled.span`
+  color: ${({ theme, $schedule }) =>
+    $schedule ? theme.colors.schedule : theme.colors.done};
+`;
